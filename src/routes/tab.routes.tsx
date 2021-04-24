89d7f86fc09918371411f0c49bb483c6bf5b7fb4
rafaelsous <rafaelsousa.souza@gmail.com>
2021-04-24 15:55:53 -0300

    Adding tab routes.

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialIcons } from '@expo/vector-icons';
+
+import { PlantSelect } from '../pages/PlantSelect';
+import { MyPlants } from '../pages/MyPlants';
+
+import fonts from '../styles/fonts'
+import colors from '../styles/colors'
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const AuthRoutes = () => (
+  <Navigator
+    tabBarOptions={{
+      activeTintColor: colors.green,
+      inactiveTintColor: colors.heading,
+      labelPosition: 'beside-icon',
+      style: {
+        height: 88,
+      }
+    }}
+  >
+    <Screen
+      name="Nova planta"
+      component={PlantSelect}
+      options={{
+        tabBarIcon: (({ size, color }) => (
+          <MaterialIcons
+            name="add-circle-outline"
+            size={size}
+            color={color}  
+          />
+        ))
+      }}
+    />
+
+    <Screen
+      name="Minhas plantinhas"
+      component={MyPlants}
+      options={{
+        tabBarIcon: (({ size, color }) => (
+          <MaterialIcons
+            name="format-list-bulleted"
+            size={size}
+            color={color}  
+          />
+        ))
+      }}
+    />
+  </Navigator>
+)
+
+export default AuthRoutes
